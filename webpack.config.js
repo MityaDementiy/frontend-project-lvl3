528d5e3b3398626405830138fd7cc0acd981520a
MityaDementiy <d.dementiy@gmail.com>
2020-02-24 16:17:36 +0200

    Updating dependencies, fixing errors (linter)

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,30 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: process.env.NODE_ENV || 'development',
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-          },
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
         },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
-        },
-      ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: 'template.html',
-      }),
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      port: 9000
-    }
-  };
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+};
